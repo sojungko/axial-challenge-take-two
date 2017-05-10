2f59ac44b41be747d9f355c5bd85b1578aec5026
Sojung Park <sojungko@gmail.com>
2017-05-10 14:02:34 -0400

    Initial commit

---
+++ src/components/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: '../templates/app.component.html',
+  styleUrls: ['../styles/app.component.css']
+})
+
+export class AppComponent {}
